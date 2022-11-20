02494cf3112a5c3ceba9a9f707bdb5c4bd1f3c1a
jdyj <jdyj@naver.com>
2022-11-20 19:32:51 +0900

    init project

---
+++ src/main/java/com/seoultech/dailydiary/DailydiaryApplication.java
@@ -0,0 +1,13 @@
+package com.seoultech.dailydiary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DailydiaryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DailydiaryApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/seoultech/dailydiary/DailydiaryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.seoultech.dailydiary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DailydiaryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
