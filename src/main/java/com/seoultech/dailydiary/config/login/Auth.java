cbb8b974c051c4628782f8f3636e26692958f049
jdyj <jdyj@naver.com>
2022-12-05 15:02:08 +0900

    [FIX] 오류 수정

--- src/main/java/com/seoultech/dailydiary/config/login/Auth.java
+++ src/main/java/com/seoultech/dailydiary/config/login/Auth.java
@@ -4,11 +4,9 @@ import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
 
 @Target(ElementType.PARAMETER)
 @Retention(RetentionPolicy.RUNTIME)
-@AuthenticationPrincipal(expression = "id")
 public @interface Auth {
 
 }
--- src/main/java/com/seoultech/dailydiary/config/login/LoginArgumentResolver.java
+++ src/main/java/com/seoultech/dailydiary/config/login/LoginArgumentResolver.java
@@ -1,35 +1,37 @@
 package com.seoultech.dailydiary.config.login;
 
+import com.seoultech.dailydiary.exception.NotExistMemberException;
+import javax.servlet.http.HttpServletRequest;
+import lombok.RequiredArgsConstructor;
 import org.springframework.core.MethodParameter;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 import org.springframework.web.bind.support.WebDataBinderFactory;
 import org.springframework.web.context.request.NativeWebRequest;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.method.support.ModelAndViewContainer;
 
+@RequiredArgsConstructor
 @Component
 public class LoginArgumentResolver implements HandlerMethodArgumentResolver {
 
+  private final HttpServletRequest httpServletRequest;
+
   @Override
   public boolean supportsParameter(MethodParameter parameter) {
-    return parameter.getParameterType() == String.class;
+
+    boolean isLoginUserAnnotation = parameter.getParameterAnnotation(Auth.class) != null;
+    boolean isStringClass = String.class.equals(parameter.getParameterType());
+
+    return isLoginUserAnnotation && isStringClass;
   }
 
   @Override
   public Object resolveArgument(MethodParameter parameter, ModelAndViewContainer mavContainer,
       NativeWebRequest webRequest, WebDataBinderFactory binderFactory) throws Exception {
-
-    Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-    Object principal = null;
-
-    if (authentication != null) {
-      principal = authentication.getPrincipal();
+    Object memberId = httpServletRequest.getAttribute("memberId");
+    if (memberId == null) {
+      throw new NotExistMemberException();
     }
-    if (principal == null) {
-      return null;
-    }
-    return principal;
+    return memberId.toString();
   }
 }
